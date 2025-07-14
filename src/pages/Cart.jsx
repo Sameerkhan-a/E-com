3a151c2da45e1d183d4e029573e2c55f27bb5ca2
Sameer Khan <sameer30khan2001@gmail.com>
2025-07-14 20:35:52 +0530

    final commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -62,6 +62,7 @@ const Cart = () => {
             <div className='w-full text-end'>
               <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3 cursor-pointer'>PROCEED TO CHECKOUT ----==========--------</button>
               <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3 cursor-pointer'>PROCEED TO Update</button>
+              <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3 cursor-pointer'>PROCEED TO Header</button>
             </div>
           </div>
         </div>
